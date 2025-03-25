4b33297159107f1a310e666d31eb93a5b34401dc
пчел <ersovavaleria215@gmail.com>
2025-03-25 15:17:57 +1000

    HW3

--- app/src/main/java/co/feip/fefu2025/MainActivity.kt
+++ app/src/main/java/co/feip/fefu2025/MainActivity.kt
@@ -3,7 +3,6 @@ package co.feip.fefu2025
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import co.feip.fefu2025.Consts.genres
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -14,23 +13,23 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.addNewFilter)
         button.setOnClickListener {
-            addNewFilter()
+          //  addNewFilter()
         }
     }
 
-    private fun addNewFilter() {
-        val customLayout = findViewById<CustomLayout>(R.id.customLayout)
-        val randomNewFilter = Filter(this).apply {
-            setItem(
-                name = genres.random(),
-                color = android.graphics.Color.argb(255,
-                    Random.nextInt(256),
-                    Random.nextInt(256),
-                    Random.nextInt(256)
-                ),
-                percentage = Random.nextFloat() * 100
-            )
-        }
-        customLayout.addView(randomNewFilter)
-    }
+//    private fun addNewFilter() {
+//        val customLayout = findViewById<CustomLayout>(R.id.customLayout)
+//        val randomNewFilter = Filter(this).apply {
+//            setItem(
+//                name = genres.random(),
+//                color = android.graphics.Color.argb(255,
+//                    Random.nextInt(256),
+//                    Random.nextInt(256),
+//                    Random.nextInt(256)
+//                ),
+//                percentage = Random.nextFloat() * 100
+//            )
+//        }
+//        customLayout.addView(randomNewFilter)
+//    }
 }

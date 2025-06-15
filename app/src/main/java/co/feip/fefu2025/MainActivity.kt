708aec5cfa4c733f74f20d83fac824d3e2044361
Lera Ershova <ersovavaleria215@gmail.com>
2025-06-15 19:57:14 +1000

    add DeepLink

--- app/src/main/java/co/feip/fefu2025/MainActivity.kt
+++ app/src/main/java/co/feip/fefu2025/MainActivity.kt
@@ -6,20 +6,57 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.remember
-import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import co.feip.fefu2025.presentation.navigation.AppNavigation
 import co.feip.fefu2025.presentation.navigation.Screen
 import co.feip.fefu2025.ui.theme.FEFU2025AndroidBaseRepoTheme
 
 class MainActivity : AppCompatActivity() {
+    private var navController: NavHostController? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             FEFU2025AndroidBaseRepoTheme {
-                val navController = rememberNavController()
-                AppNavigation(navController = navController)
+                navController = rememberNavController()
+                navController?.let { controller ->
+                    AppNavigation(navController = controller)
+                }
+            }
+        }
+
+        handleIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent?) {
+        if (intent?.action == Intent.ACTION_VIEW) {
+            val uri = intent.data
+            if (uri != null) {
+                handleDeepLink(uri)
+            }
+        }
+    }
+
+    private fun handleDeepLink(uri: Uri) {
+        if (uri.scheme == "mysuperapp" && uri.host == "anime") {
+            val pathSegments = uri.pathSegments
+            if (pathSegments.isNotEmpty()) {
+                val animeId = pathSegments[0].toIntOrNull()
+                if (animeId != null) {
+                    navController?.navigate(Screen.detailsRoute(animeId)) {
+                        popUpTo(Screen.Main.route) {
+                            inclusive = false
+                        }
+                    }
+                }
             }
         }
     }
